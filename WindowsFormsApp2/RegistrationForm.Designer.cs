500cdc5a9875a7e2de3dd2f5dbb3afcb22b942fc
tima7081zzz <76428163+tima7081zzz@users.noreply.github.com>
2021-05-18 22:30:26 +0300

    Add files via upload

---
+++ WindowsFormsApp2/RegistrationForm.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class RegistrationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.PassBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PhoneBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.AdressBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.Controls.Add(this.buttonRegister);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.PassBox);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.PhoneBox);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.AdressBox);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.NameBox);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(614, 406);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.buttonRegister.Font = new System.Drawing.Font("Adobe Gothic Std B", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonRegister.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.buttonRegister.Location = new System.Drawing.Point(202, 309);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(232, 72);
+            this.buttonRegister.TabIndex = 3;
+            this.buttonRegister.Text = "CREATE ACCOUNT";
+            this.buttonRegister.UseVisualStyleBackColor = false;
+            this.buttonRegister.Click += new System.EventHandler(this.buttonRegister_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Adobe Gothic Std B", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label5.Location = new System.Drawing.Point(202, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 20);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Password";
+            this.label5.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // PassBox
+            // 
+            this.PassBox.Location = new System.Drawing.Point(288, 261);
+            this.PassBox.Name = "PassBox";
+            this.PassBox.Size = new System.Drawing.Size(135, 23);
+            this.PassBox.TabIndex = 1;
+            this.PassBox.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Adobe Gothic Std B", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label4.Location = new System.Drawing.Point(131, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Telephone Number";
+            this.label4.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // PhoneBox
+            // 
+            this.PhoneBox.Location = new System.Drawing.Point(288, 220);
+            this.PhoneBox.Name = "PhoneBox";
+            this.PhoneBox.Size = new System.Drawing.Size(135, 23);
+            this.PhoneBox.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Adobe Gothic Std B", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label3.Location = new System.Drawing.Point(223, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Adress";
+            this.label3.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // AdressBox
+            // 
+            this.AdressBox.Location = new System.Drawing.Point(288, 177);
+            this.AdressBox.Name = "AdressBox";
+            this.AdressBox.Size = new System.Drawing.Size(135, 23);
+            this.AdressBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Adobe Gothic Std B", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Location = new System.Drawing.Point(192, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Your Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(288, 134);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(135, 23);
+            this.NameBox.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(614, 108);
+            this.panel2.TabIndex = 0;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Adobe Gothic Std B", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(134, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(349, 60);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "REGISTRATION";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // RegistrationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 406);
+            this.Controls.Add(this.panel1);
+            this.Name = "RegistrationForm";
+            this.Text = "RegistrationForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox PhoneBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox AdressBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox PassBox;
+        private System.Windows.Forms.Button buttonRegister;
+    }
+}
---
+++ WindowsFormsApp2/RegistrationForm.cs
@@ -0,0 +1,127 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class RegistrationForm : Form
+    {
+        public object MySqlcommand { get; private set; }
+
+        public RegistrationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRegister_Click(object sender, EventArgs e)
+        {
+            if(NameBox.Text == "")//condition of fillinf of the field
+            {
+                MessageBox.Show("Type your name");
+                return;
+            }
+
+            if (AdressBox.Text == "")//condition of fillinf of the field
+            {
+                MessageBox.Show("Type your adress");
+                return;
+            }
+
+            if (PhoneBox.Text == "")//condition of fillinf of the field
+            {
+                MessageBox.Show("Type your phone number");
+                return;
+            }
+
+            if (PassBox.Text == "")//condition of fillinf of the field
+            {
+                MessageBox.Show("Create a password");
+                return;
+            }
+
+            if(checkUser())//condition of sameness of the passwords
+            {
+                MessageBox.Show("This password already exist \nCreate another password");
+                return;
+            }
+
+
+            //creating acount for regestrated user
+
+            DB db = new DB();
+            MySqlCommand command = new MySqlCommand("INSERT INTO `user` (`id`, `name`, `adress`, `phone`, `password`) VALUES (NULL, @nam, @adr, @phon, @pass)", db.getConnection());
+
+            command.Parameters.Add("@nam", MySqlDbType.VarChar).Value = NameBox.Text;
+            command.Parameters.Add("@adr", MySqlDbType.VarChar).Value = AdressBox.Text;
+            command.Parameters.Add("@phon", MySqlDbType.VarChar).Value = PhoneBox.Text;
+            command.Parameters.Add("@pass", MySqlDbType.VarChar).Value = PassBox.Text;
+
+            db.openConnection();
+
+            if(command.ExecuteNonQuery()==1)
+            {
+                MessageBox.Show("Account was created");
+                this.Hide();
+                LoginForm registrationForm = new LoginForm();
+                registrationForm.Show();
+            }
+            else
+            {
+                MessageBox.Show("Acoount wasn`t created");
+            }
+
+
+
+            db.closeConnection();
+        }
+
+        public bool checkUser()//checking of sameness
+        {
+            DB db = new DB();
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `user` WHERE `password` = @uP", db.getConnection());
+            command.Parameters.Add("@uP", MySqlDbType.VarChar).Value = PassBox.Text;
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
